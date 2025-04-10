0d21b9ac3ba97bd7b9178a6498b983afeb650074
lcb <lennart.carbe@gmail.com>
2025-04-10 13:05:04 +0200

    health route

--- app/api/routes/v1/__init__.py
+++ app/api/routes/v1/__init__.py
@@ -2,6 +2,8 @@
 from fastapi import APIRouter
 
 from .timelines import router as timelines_router
+from .fediway import router as fediway_router
 
 router = APIRouter()
+router.include_router(fediway_router, prefix='/fediway')
 router.include_router(timelines_router, prefix='/timelines')
