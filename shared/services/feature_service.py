5a7d4d74cf123d8f805893bbf91b02e2e2a2ee1c
lcb <lennart.carbe@gmail.com>
2025-05-12 10:57:43 +0200

    wip

--- shared/services/feature_service.py
+++ shared/services/feature_service.py
@@ -15,6 +15,49 @@ class FeatureService(Features):
     def __init__(self, fs: FeatureStore = feature_store):
         self.fs = fs
 
+    def _cache_key(self, entities: list[dict[str, int]]) -> str:
+        return ",".join(list(entities[0].keys()))
+
+    def _get_cached(self, entities: list[dict[str, int]], features: list[str]):
+        cache_key = self._cache_key(entities)
+
+        if not cache_key in self.cache:
+            return None, entities
+
+        missing_entities = []
+        entity_ids = pd.DataFrame(entities).values[:, 0]
+        cached = []
+
+        for feature in features:
+            if not feature in self.cache[cache_key]:
+                return None, entities
+            cached.append(self.cache[cache_key][feature].reindex(entity_ids))
+
+        cached = pd.concat(cached, axis=1, join='inner')
+        cached_entities = set(cached.index)
+
+        return cached, [{cache_key: entity} for entity in entity_ids if not entity in cached_entities]
+
+    def _remember(self, entities, df, features):
+        cache_key = self._cache_key(entities)
+
+        if "," in cache_key:
+            return
+        
+        entity_ids = pd.DataFrame(entities).values[:, 0]
+
+        if not cache_key in self.cache:
+            self.cache[cache_key] = {}
+
+        for column, feature_name in zip(df.columns, features):
+            feat = df[column]
+            feat.index = entity_ids
+            if not feature_name in self.cache[cache_key]:
+                self.cache[cache_key][feature_name] = feat
+            else:
+                feat = feat[~feat.index.isin(self.cache[cache_key][feature_name].index)]
+                self.cache[cache_key][feature_name] = pd.concat([self.cache[cache_key][feature_name], feat]).groupby(level=0).last()
+
     def get(self, entities: list[dict[str, int]], features: list[str]) -> np.ndarray | None:
         if len(features) == 0:
             return []
@@ -24,11 +67,23 @@ class FeatureService(Features):
 
         start = time.time()
 
-        data = self.fs.get_online_features(
+        cached_df, missing_entities = self._get_cached(entities, features)
+
+        if len(missing_entities) == 0 and cached_df is not None:
+            logger.info(f"Fetched cached features for {len(entities)} entities in {int((time.time() - start) * 1000)} milliseconds.")
+            return cached_df.reset_index().values
+
+        df = self.fs.get_online_features(
             features=features,
-            entity_rows=entities
-        ).to_df().values
+            entity_rows=missing_entities
+        ).to_df()
+
+        self._remember(missing_entities, df, features)
+
+        if cached_df is not None:
+            df.index = pd.DataFrame(missing_entities).values[:, 0]
+            df = pd.concat([df, cached_df]).reindex(pd.DataFrame(entities).values[:, 0])
 
         logger.info(f"Fetched features for {len(entities)} entities in {int((time.time() - start) * 1000)} milliseconds.")
 
-        return data
+        return df.values
