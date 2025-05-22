1c515b72bda6525fe2e0f1aee63290dfef4b6bd1
lcb <lennart.carbe@gmail.com>
2025-05-22 16:14:40 +0200

    fix imports

--- modules/fediway/rankers/kirby/kirby.py
+++ modules/fediway/rankers/kirby/kirby.py
@@ -5,7 +5,6 @@ import numpy as np
 import pandas as pd
 from pathlib import Path
 from lightgbm import LGBMClassifier, Booster
-from xgboost import XGBClassifier
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
@@ -82,6 +81,7 @@ class Kirby(Ranker):
         scaler: str = "standard",
         random_state: int = None,
     ):
+        from xgboost import XGBClassifier
         model = XGBClassifier(
             objective="multi:softprob",
             num_class=len(labels),
