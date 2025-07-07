dc4e6a2a7d9d2a47cdb0ee7e6defa12f1fbf20de
lcb <lennart.carbe@gmail.com>
2025-07-07 15:53:58 +0200

    fix

--- apps/api/middlewares/oauth_middleware.py
+++ apps/api/middlewares/oauth_middleware.py
@@ -7,37 +7,30 @@ from shared.core.db import db_session
 
 class OAuthMiddleware:
     async def __call__(self, request: Request, call_next: callable):
-        # auth_header: str = request.headers.get("Authorization") or ""
-        # scheme, _, token = auth_header.partition(" ")
+        auth_header: str = request.headers.get("Authorization") or ""
+        scheme, _, token = auth_header.partition(" ")
 
-        # if scheme.lower() != "bearer" or not token:
-        #     return await call_next(request)
-
-        # with db_session() as db:
-        #     query = (
-        #         select(AccessToken)
-        #         .where(AccessToken.token == token)
-        #         .where(AccessToken.revoked_at.is_(None))
-        #     )
-
-        #     token = db.exec(query).one_or_none()
-
-        # if token is None:
-        #     return await call_next(request)
-
-        # request.state.token = token
-
-        # with db_session() as db:
-        #     query = select(Account).join(
-        #         User,
-        #         (User.account_id == Account.id) & (User.id == token.resource_owner_id),
-        #     )
-
-        #     account = db.exec(query).one_or_none()
+        if scheme.lower() != "bearer" or not token:
+            return await call_next(request)
 
         with db_session() as db:
-            query = select(Account).where(
-                (Account.username == "fediway") & (Account.domain == None)
+            query = (
+                select(AccessToken)
+                .where(AccessToken.token == token)
+                .where(AccessToken.revoked_at.is_(None))
+            )
+
+            token = db.exec(query).one_or_none()
+
+        if token is None:
+            return await call_next(request)
+
+        request.state.token = token
+
+        with db_session() as db:
+            query = select(Account).join(
+                User,
+                (User.account_id == Account.id) & (User.id == token.resource_owner_id),
             )
 
             account = db.exec(query).one_or_none()
